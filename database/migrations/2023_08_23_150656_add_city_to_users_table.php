168ccca26a2ca1d5c565075867ee372bb03d60f0
Mirai <b1021135@fun.ac.jp>
2023-08-23 17:38:42 +0900

    空欄許可を削除

--- database/migrations/2023_08_23_150656_add_city_to_users_table.php
+++ database/migrations/2023_08_23_150656_add_city_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('city')->nullable();
+            $table->string('city');
         });
     }
 
