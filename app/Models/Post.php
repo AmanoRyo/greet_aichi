5b4e67e460c649c5b449099610d0e09dd7c10f28
AmanoRyo <amano.ryo.yt@tut.jp>
2023-08-24 19:11:17 +0900

    コンフリクト再解消

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,35 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    protected $targetLanguage;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->targetLanguage = auth()->user()->language;
+    }
+   
+
+    public function translateText($text, $targetLanguage)
+    {
+        $apiKey = config('services.google.translate.key');
+        $translate = new TranslateClient(['key' => $apiKey]);
+        $result = $translate->translate($text, ['target' => $targetLanguage]);
+        return $result['text'];
+    }
+
+    public function getTitleAttribute($value)
+    {
+        $targetLanguage = auth()->user()->language;
+        return $this->translateText($value, $targetLanguage);
+    }
+
+    public function getBodyAttribute($value)
+    {
+        $targetLanguage = auth()->user()->language;
+        return $this->translateText($value, $targetLanguage);
+    }
+    
     public function answers()
     {
         return $this->hasMany(AnswerPost::class);
